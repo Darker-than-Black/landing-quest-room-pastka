d4fbb89fb049cb496dbcdf518632de58fb6702f9
Rybachek Dmytro <puss.in.boots@i.ua>
2019-02-22 00:18:46 +0200

    fix mobile layout, add modal window and playloader, fix parallax effect

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 "use strict";
-// Smooth scroll to anchor
-
-// var time = 2000;
 if( navigator.userAgent.search('Edge') >= 0 ) {
 	$('.header__nav')[0].style.display = "grid";
 }
@@ -13,100 +10,131 @@ if( $(window).width() > 992 ) {
 }
 
 $('.tabs__table')[0].style.display = table;
+
 function openTabs(e, room, num) {
+	$('.loader').css({
+		'display': 'flex'
+	});
+
 	for(var i = 0; i < $('.tabs__table').length; i++) {
 		$('.tabs__table')[i].style.display = "none";
 		$('.tab--link')[i].classList.remove("openTab");
 	}
 
 	$('.tab--link')[num].classList.add("openTab");
-	$('#' + room).css("display", table)
-}
+	$('#' + room).css("display", table);
+
+	setTimeout(function () {
+		$('.loader').css({
+			'display': 'none'
+		});
+	}, 500);
+};
 
 $( document ).ready(function() {
-	$('.header__nav a[href*="#"], .header__btn-container a[href*="#"], .quest__qroup a[href*="#"], .in_game__btn a[href*="#"]')
-		.not('[href="#"]')
-		.not('[href="#0"]')
-		.click(function(event) {
-		var time =  Math.abs( ($("#" + event.target.dataset.path).offset().top - $(this).offset().top ) / 2.5 );
+	$('.header__nav a[href*="#"], .header__btn-container a[href*="#"], .quest__qroup a[href*="#"], .in_game__btn a[href*="#"], .footer__nav--site a[href*="#"]')
+	.on('click', function(event) {
+		$('.loader').css({
+			'display': 'flex'
+		});
 
-		if (
-		  location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-		  && 
-		  location.hostname == this.hostname
-		) {
-		  var target = $(this.hash);
-		  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-		  if (target.length) {
-		    event.preventDefault();
-		    $('html, body').animate({
-		      scrollTop: target.offset().top
-		    }, time, function() {
-		      var $target = $(target);
-		      $target.focus();
-		      if ($target.is(":focus")) {
-		        return false;
-		      } else {
-		        $target.attr('tabindex','-1');
-		        $target.focus();
-		      };
-		    });
-		  }
-		}
+
+		setTimeout(function () {
+			$('.loader').css({
+				'display': 'none'
+			});
+		}, 1000);
 	});
+
+	let data = {
+		title: null,
+		gameTime: null,
+		available: null,
+		time: null,
+		form: {
+			count: 1,
+			price: null,
+			name: null,
+			phone: null,
+			email: null,
+			comment: null,
+			totalPrice: null,
+		}
+	};
 
 
 	$('.booking__free').click(function(e) {
-		console.log(e)
-
 		if( e.target.className == 'booking__free' ) {
-			console.log( e.target.dataset.time )
+			data.title = $.trim( $('#'+e.target.dataset.room+' .quest__title').text() );
+			data.gameTime = $('#'+e.target.dataset.room).data('time');
+			data.available = $('#'+e.target.dataset.room).data('available');
+			data.form.price = $('#'+e.target.dataset.room).data('price');
+			data.time = e.target.dataset.time;
+
+			$('.modal .quest__title').text( data.title );
+			$('#reserve_date').text( data.time );
+			$('#minutes').text( data.gameTime );
+			$('#available').text( data.available + '/' + data.available );
+			$('#price').text( data.form.price );
+			data.form.count = 1;
+			setCountVal();
+
+			for(let i = 0; i < $('.modal .quest__row .col-12:first-child').children().length; i++) {
+				$('.modal .quest__row .col-12:first-child').children()[i].style.display = 'none';
+			}
+
+			if(e.target.dataset.room == 'quests') {
+				$('.modal .quest__row .col-12:first-child').children()[0].style.display = 'block';
+			} else if(e.target.dataset.room == 'quests_2') {
+				$('.modal .quest__row .col-12:first-child').children()[1].style.display = 'block';
+			} else {
+				$('.modal .quest__row .col-12:first-child').children()[2].style.display = 'block';
+			}
+
+			$('.modal').fadeIn(200);
+			setTimeout(function () {
+				$('.modal .quest').fadeIn(700);
+			}, 250);
 		}
 	});
 
+	let lateScrollingArray = [];
+	let st = 0;
+	$(window).on('scroll',function(e){
+		st = $(this).scrollTop();
 
-	var lateScrollingArray = [];
-	$(window).scroll(function(e){
-		var st = $(this).scrollTop();
-
-		$(".in_game .triangle--big").css({
-		  "transform" : "translate(0%, " + st*0.05 + "%" 
+		$('.in_game .triangle--big').css({
+			transform: 'translateY(' + st * 0.1 + 'px)',
 		});
 
 		$(".in_game .triangle.triangle--smallUp").css({
-		  "bottom" : + 23 - st*0.03 + "vh",
-		  "transform" : " rotate(" + (st*0.04-25) + "deg)"
-		}); 
+			transform: 'translateY(' + st * 0.175 +  'px) rotate(-25deg)',
+		});
 
 		$(".in_game .triangle.triangle--smallDown").css({
-		  "bottom" : + 7 - st*0.03 + "vh",
-		  "transform" : " rotate(" + (-st*0.07-52) + "deg)"
+			transform: 'translateY(' +  ( -st * 0.13 ) + 'px) rotate(-52deg)',
 		});
 
 		$(".quest .triangle--big").css({
-		  "top" : + 2+st*0.02 + "vh",
+			transform: 'translateY(' + st * 0.2 + 'px) rotate(180deg)',
 		});
-
 
 		if(  $(this).scrollTop() >= $('.header').height() + $('.in_game').height() ) {
 			if( !lateScrollingArray[0] ) {
 				lateScrollingArray[0] = st;
 			}
 			$('.quest__legion--two .triangle.triangle--smallDown').css({
-			  "top" : + (10+(st-lateScrollingArray[0])*0.03) + "vh",
-			  "transform" : " rotate(" + (st*0.1+3) + "deg)"
+				transform: 'translateY(' + (st - lateScrollingArray[0]) * 0.075 + "px) rotate(3deg)",
 			});
 		}
 
 		if(  $(this).scrollTop() >= $(".quest__legion--two").offset().top ) {
-			// console.log('fghj')
 			if( !lateScrollingArray[1] ) {
 				lateScrollingArray[1] = st;
 			}
 			$('.quest__legion--three .triangle.triangle--smallDown').css({
-			  "top" : + (10+(st-lateScrollingArray[1])*0.03) + "vh",
-			  "transform" : " rotate(" + (st*0.1+3) + "deg)",
-			  "z-index" : "-1"
+				transform: 'translateY(' + (st - lateScrollingArray[1]) * (-0.22) + "px) rotate(21deg)",
+		  		"z-index" : "-1"
 			});
 		}
 
@@ -115,9 +143,8 @@ $( document ).ready(function() {
 				lateScrollingArray[2] = st;
 			}
 			$('.contacts .triangle.triangle--smallDown').css({
-			  "top" : + (-5+(st-lateScrollingArray[2])*0.04) + "vh",
-			  "transform" : " rotate(" + (st*0.05+3) + "deg)",
-			  "z-index" : "-1"
+				transform: 'translateY(' + (st - lateScrollingArray[2]) * 0.15 + "px) rotate(35deg)",
+		  		"z-index" : "-1"
 			});
 		}
 
@@ -126,17 +153,74 @@ $( document ).ready(function() {
 				lateScrollingArray[3] = st;
 			}
 			$('.contacts .triangle.triangle--smallUp').css({
-			  "bottom" : + (14-(st-lateScrollingArray[3])*0.03) + "vh",
-			  "transform" : " rotate(" + (st*0.07+3) + "deg)",
-			  "z-index" : "9"
+				transform: 'translateY(' + (st - lateScrollingArray[3]) * (-0.175) + "px) rotate(17deg)",
+		  		"z-index" : "-9"
 			});
 		}
-
-
-
-		// console.log( $('.header').height() + $('.in_game').height()  )
-		// console.log( $(".quest .triangle--big").offset()  )
-
 	});
-    
+
+	let answerText = null;
+	$('#reserve__room').on('submit', function (e) {
+		e.preventDefault();
+		data.form.name = e.target[0].value;
+		data.form.phone = e.target[1].value;
+		data.form.email = e.target[2].value;
+		data.form.comment = e.target[3].value;
+
+		$.post('https://httpbin.org/post', {
+			title: data.title,
+			time: data.time,
+			name: data.form.name,
+			phone: data.form.phone,
+			email: data.form.email,
+			comment: data.form.comment,
+			numberOfPlayers: data.form.count,
+			totalPrice: data.form.totalPrice
+		}).done(function() {
+			answerText = 'Запрос успешен';
+		}).fail(function() {
+			answerText = 'Случилось ошибка, спробуйте еще раз';
+		}).always(function() {
+			$('.modal .modal__wrapper .modal__conteiner').text( answerText );
+			$('.modal .quest').fadeOut(500);
+			setTimeout(function () {
+				$('.modal .modal__wrapper').fadeIn(500);
+			}, 550);
+		});
+	});
+	setCountVal();
+
+	function setCountVal() {
+		$('#count').text( data.form.count );
+		data.form.totalPrice = data.form.count * data.form.price;
+		$('#totalCount').text( data.form.totalPrice );
+	};
+
+	$('#minus').click(function () {
+		if(data.form.count > 1)
+			data.form.count--;
+		setCountVal();
+	});
+	$('#plus').click(function () {
+		if(data.form.count < 6)
+			data.form.count++;
+		setCountVal();
+	});
+
+	$('#close').click(function (e) {
+		e.preventDefault();
+		$('.modal .quest').fadeOut(700);
+		$('.modal .modal__wrapper').fadeOut(700);
+		setTimeout(function () {
+			$('.modal').fadeOut(200);
+		}, 750);
+	})
+});
+
+$( window ).on( "load", function() {
+	setTimeout(function () {
+		$('.loader').css({
+			'display': 'none'
+		})
+	}, 500);
 });
